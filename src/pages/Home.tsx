828a0c7a7fc656b42f375b026b6fe429677c1a70
Roberts Dzerins <romadi@inbox.lv>
2025-09-28 17:51:52 +0300

    sitmeap

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,16 +13,12 @@ export default function Home() {
             name="description"
             content="Romadi izstrādā pielāgotas mājaslapas, pārdomātu dizainu un SEO risinājumus uzņēmumiem Latvijā. Tehniski precīzi, vizuāli pārliecinoši, ar rezultātu prātā."
             />
-            <meta
-            property="og:title"
-            content="Pielāgotas mājaslapas, dizains un SEO – Romadi"
-            />
-            <meta
-            property="og:description"
-            content="Romadi palīdz uzņēmumiem augt tiešsaistē – ar web dizainu, izstrādi un digitālo stratēģiju. Saskarē ar kvalitāti."
-            />
-            <meta property="og:image" content="https://romadi.lv/og/home.jpg" />
-            <meta property="og:url" content="https://romadi.lv/" />
+        <meta name="robots" content="index, follow, max-snippet:-1" />
+        <link rel="canonical" href="https://romadi.lv/" />
+        <meta property="og:title" content="Pielāgotas mājaslapas, dizains un SEO – Romadi" />
+        <meta property="og:description" content="Romadi palīdz uzņēmumiem augt tiešsaistē – ar web dizainu, izstrādi un digitālo stratēģiju. Saskarē ar kvalitāti." />
+        <meta property="og:image" content="https://romadi.lv/og/home.jpg" />
+        <meta property="og:url" content="https://romadi.lv/" />
         </Helmet>
             <Hero />
             <Services />
